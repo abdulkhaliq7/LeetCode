e5cd8f3d407c45ce0a1b6ca805c3723abeb5e984
abdulkhaliq7 <abdikhaliq_7@hotmail.com>
2024-01-15 11:44:43 +0300

    added isAnagram solution

--- isAnagram/isAnagram.go
+++ isAnagram/isAnagram.go
@@ -2,8 +2,36 @@ package main
 
 import "fmt"
 
+func isAnagram(s string, t string) bool {
+	if len(s) != len(t) {
+		return false
+	}
 
-func main () {
-	x:=5
-	fmt.Println(x)
+	freqs := make(map[rune]int)
+
+	freqt := make(map[rune]int)
+
+	for _, s1 := range s {
+		freqs[s1]++
+	}
+
+	for _, t1 := range t {
+		freqt[t1]++
+	}
+
+	for char, count := range freqs {
+		if freqt[char] != count {
+			return false
+		}
+	}
+
+	return true
+
+}
+
+func main() {
+	s := "halp"
+	t := "hala"
+
+	fmt.Println(isAnagram(s, t))
 }
