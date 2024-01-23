49fb0375a9e0400616b0242fa6ea95a6f8a13a17
abdulkhaliq7 <abdikhaliq_7@hotmail.com>
2024-01-23 12:06:44 +0300

    added final validsudoku solution

--- validSudoku/validSudoku.go
+++ validSudoku/validSudoku.go
@@ -4,29 +4,38 @@ import (
 	"fmt"
 )
 
-func isValidSudoku(board [][]byte) bool {
 
-	col := [9][9]bool{}
+func isValidSudoku(board [][]byte) bool { 
 
-	for i := 0; i < len(board); i++ {
-		for j := 0; j < len(board); j++ {
-
-			if col[i][board[i][j]] {
-				return false
-			}
-			col[i][board[i][j]] = true
-		}
-	}
-
-	return true
+    var rows, columns, squares [9][9]bool
+    for i, row := range board {
+        for j, v := range row {
+            if v != '.' {
+                k := int(v)-49
+                if rows[i][k] || columns[j][k] || squares[i/3*3 + j/3][k] {
+                    return false
+                }
+                rows[i][k], columns[j][k], squares[i/3*3 + j/3][k] = true, true, true
+            }
+        }
+    }
+    return true
 }
 
 func main() {
 
-	sudokuMap := [][]byte{
-		{1, 2, 3, 8, 4, 6, 7, 8, 9},
-	}
+	sudokuBoard := [][]byte{
+        {'5', '3', '.', '.', '7', '.', '.', '.', '.'},
+        {'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+        {'.', '9', '8', '.', '.', '.', '.', '6', '.'},
+        {'8', '.', '.', '.', '6', '.', '.', '.', '3'},
+        {'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+        {'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+        {'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+        {'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+        {'7', '.', '.', '.', '8', '.', '.', '7', '9'},
+    }
 
-	fmt.Println(isValidSudoku(sudokuMap))
+	fmt.Println(isValidSudoku(sudokuBoard))
 
 }
