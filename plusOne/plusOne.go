36b3178d01398702cae4719e3c3d007d9ed54901
abdulkhaliq7 <abdikhaliq_7@hotmail.com>
2024-02-08 10:50:44 +0300

    added plusOne solution and description

---
+++ plusOne/plusOne.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+)
+
+func plusOne(digits []int) []int {
+    
+    for i:=len(digits)-1; i >=0 ; i-- {
+        if digits[i] < 9 {
+            digits[i]++
+            return digits
+        }
+
+        digits[i] = 0
+    }
+
+    digits = append([]int{1}, digits...)
+
+    return digits
+}
+
+func main() {
+
+	digits := []int{9, 9, 9}
+
+    fmt.Println(plusOne(digits))
+}
