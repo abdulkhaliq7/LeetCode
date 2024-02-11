ff0797c651043b44d96546774aecb0602e2b4a56
abdulkhaliq7 <abdikhaliq_7@hotmail.com>
2024-02-11 10:22:38 +0300

    added powerOfFour

---
+++ powerOfFour/powerOfFour.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+)
+
+
+func isPowerOfFour(n int) bool {
+
+	if n == 1 {
+		return true
+	}
+	if n <= 0 || n % 4 != 0 {
+		return false
+	}
+	 
+	 return isPowerOfFour(n / 4)
+	
+	}
+
+func main() {
+	fmt.Println(isPowerOfFour(8))
+
+}
+	
